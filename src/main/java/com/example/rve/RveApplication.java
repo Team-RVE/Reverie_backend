d2922065b526cd375f7ad7e3fb5444326e951165
ihansaem <yellowstarr@dsm.hs.kr>
2025-09-30 18:11:14 +0900

    init

---
+++ src/main/java/com/example/rve/RveApplication.java
@@ -0,0 +1,13 @@
+package com.example.rve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RveApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RveApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/rve/RveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RveApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
